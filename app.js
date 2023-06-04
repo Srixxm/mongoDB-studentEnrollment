3979897bdae146d2c807d4ca53dff70442e7b941
Srixxm <118879168+Srixxm@users.noreply.github.com>
2023-06-04 23:43:16 +0530

    updated patch and delete functions

--- app.js
+++ app.js
@@ -7,19 +7,18 @@ const mongoose = require('mongoose')
 
 app.use(express.json())
 
-
 mongoose.connect(process.env.my_db)
 const db = mongoose.connection
 db.on('error', errorMessage => console.log(errorMessage))
 db.once('open', ()=> console.log('connection established'))
 
 
-app.use('/api/v1/students', Stud)
-
 
 app.get(('/'), (request, response) => {
     response.send("welcome")
 })
+
+app.use('/api/v1/students', Stud)
 
 
 app.listen(PORT, console.log('listening on port http://localhost:3880/'))
--- routes/students.js
+++ routes/students.js
@@ -12,6 +12,10 @@ router.get('/', async (request, response) => {
     }
 })
 
+router.get('/:id', getStudent ,(request,response)=>{
+    response.status(200).json(response.student)
+})
+
 router.post('/', async (request, response) => {
     const newStudent = new studentsModel({
         name:  request.body.name,
@@ -27,16 +31,37 @@ router.post('/', async (request, response) => {
     }
 })
 
-router.get('/:id', getStudent,(request,response)=>{
-    response.status(200).json(response.student)
+
+
+router.patch(('/:id'), getStudent, async(request, response) => {
+    // response.send(`Updating Student informations by id : ${request.params.id}`)
+    if (request.body.name != null){
+        response.student.name = request.body.name
+    }
+    if (request.body.enrollDepartment != null){
+        response.student.enrollDepartment = request.body.enrollDepartment
+    }
+
+    
+    try{
+        const updateStudent = await response.student.save() // saving the details sent by user
+        response.status(201).json(updateStudent)
+    }
+    catch (error){
+        response.status(400).json({message:error.message})
+    }
+
 })
 
-router.patch(('/:id'), (request, response) => {
-    response.send(`Updating Student informations by id : ${request.params.id}`)
-})
-
-router.delete(('/:id'), (request, response) => {
-    response.send(`Deleting Student informations by id : ${request.params.id}`)
+router.delete(('/:id'), getStudent ,async (request, response) => {
+    // response.send(`Deleting Student informations by id : ${request.params.id}`)
+    try{
+        await response.student.deleteOne()
+        response.json({message:`Deleted ${response.student.name} `})
+    }
+    catch(error){
+        response.status(500).json({message:error.message})
+    }
 })
 
 
@@ -44,10 +69,13 @@ async function getStudent (request,response,next){
     let student 
     try{
         student = await studentsModel.findById(request.params.id)
-        if(student = null){
+        if(student == null){
             return response.status(404).json({message:`cannot find the requested user with id ${request.params.id}`})
         }
+        response.student = student 
+    next()
     }
+    
     catch(error) {
         return response.status(500).json({message:'error message'})
     }
